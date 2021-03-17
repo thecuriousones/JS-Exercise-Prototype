c5fdb1ce42e446a929c95ebbb1caed7f63fccbab
thecuriousones <tamaraaleonard46@gmail.com>
2021-03-17 00:47:43 -0400

    done

--- index.js
+++ index.js
@@ -39,11 +39,22 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age= age;
+    this.stomach =[];
   }
- 
- 
+
+  Person.prototype.eat = function(edible) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(edible);
+    }};
+  Person.prototype.poop = function() {
+      this.stomach = [];
+    };
+  Person.prototype.toString = function() {
+      return `${this.name}, ${this.age}`;
+    }
 
   
   
@@ -63,9 +74,18 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
+ function Car(model, milesPerGallon) {
+   this.model = model;
+   this.milesPerGallon = milesPerGallon;
+   this.tank = 0;
+   this.odometer = 0;
     
   }
+
+  Car.prototype.fill = function(gallons) {
+    this.tank = gallons + this.tank;
+  };
+  
   
   
   /*
@@ -75,18 +95,25 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   this.name = name;
+   this.age = age;
+   this.favoriteToy = favoriteToy;
   }
- 
+
+  Baby.prototype = Object.create(Person.prototype);
+  
+  Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`
+  }
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Principle 1 which is global scope value of this would be the entire window object.
+    2. Princle 2 which is implicit binding would be when you use the dot syntax to define the value of this.
+    3. Principle 3 which is New Bining, it defines the value of 'this' with the specific instances of an object inside a constructor function.
+    4. Principle 4 which is Explicit Binding which is whenever we use .bind, .call, or .apply.
   */
   
   
